c152bae144b2ab59ccaed7668d121b3fac082c3c
jose <jgabrielll2007@gmail.com>
2025-05-09 19:26:46 -0300

    Createdivisoo_resto.py

---
+++ logica de programação/aula02/divisoo_resto.py
@@ -0,0 +1,8 @@
+num1 = int(input('Qual o primiro numro'))
+num2 = int(input('Qual o segundo numro'))
+
+div_int = num1 // num2
+resto = num1 % num2
+
+print('A divisão inteira é: ',div_int)
+print('o resto da divisão é: ',resto)
