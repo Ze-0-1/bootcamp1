7be5d3032d61245c9e8ab2b1a6dd62eda53b6a94
jose <jgabrielll2007@gmail.com>
2025-05-10 00:34:48 -0300

    Createteste_logico.py

---
+++ logica de programação/aula05/teste_logico.py
@@ -0,0 +1,29 @@
+a = 10
+b = 5
+
+print(a == b)
+# False
+print(a != b)
+# True
+
+print()
+
+
+print(a == b and a != b)
+# False
+
+print(a == b or a != b)
+# True
+
+print(not a == b)
+# True
+
+# Tabela verdade do "e" / “and” feito no print
+# Está conforme pedido nos exercícios
+
+print("Tabela Verdade do AND")
+
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
